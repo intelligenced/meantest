4495bbdddda89cb582f5f2258e805b72bb4e8046
intelligenced <intelligenced@gmail.com>
2015-12-28 14:02:06 +0500

    ui router added again

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-var app = angular.module('flapperNews', []);
+angular.module('flapperNews', ['ui.router']);
+
+app.config(['$stateProvider','$urlRouterProvider', function($stateProvider,$urlRouterProvider){
+$stateProvider.state('home',{url:'.home',templateUrl:'/home.html', controller:'MainCtrl'});
+
+$urlRouterProvider.otherwise('home');
+
+
+}]);
 
 app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts){
 
